d6f5570b5c6df4739fd7ee0f6da3cd5997a800b9
MikeWolski <99278593+MikeWolski@users.noreply.github.com>
2022-02-23 11:31:56 -0500

    Update movies.js

--- src/utils/movies.js
+++ src/utils/movies.js
@@ -1,4 +1,14 @@
 // Your movies here
-const movies = [];
+const movies = [{
+    title: "Mike Wolski's Movie",
+    description: "The BEST movie EVER!",
+    imageURL: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/51aae682-4c75-4087-a466-805dfe17aaeb/de6lrff-b4993aa8-855c-4e3a-9fc2-5a0d30f7fb51.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzUxYWFlNjgyLTRjNzUtNDA4Ny1hNDY2LTgwNWRmZTE3YWFlYlwvZGU2bHJmZi1iNDk5M2FhOC04NTVjLTRlM2EtOWZjMi01YTBkMzBmN2ZiNTEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.w2tdvGVO5ZO3VfosBrys4A5V09MNAr2n86harOykb8c",
+    releaseYear: "2022",
+    contentRating: "PG-13",
+    runTime: "1:30",
+    directors: ["Mike Wolski"],
+    writers: ["Mike Wolski"],
+    actors: ["Mike Wolski", "Mike Wizowski"]
+}];
 
 export default movies;
